026e80db25887254e62a496802ee5e1cf16097f1
TatianaKoma <tatiana78977ua@gmail.com>
2021-09-04 22:38:42 +0300

    homework 7 was completed

--- src/lesson7/homework/table_homework.sql
+++ src/lesson7/homework/table_homework.sql
@@ -66,7 +66,7 @@ CREATE TABLE student
 (
     id BIGSERIAL PRIMARY KEY ,
     full_name VARCHAR,
-    level INT DEFAULT '1',
+    level INT DEFAULT 1,
 
     CONSTRAINT CHK_student_level CHECK ( level = '1' OR level = '2' OR level = '3')
 );
@@ -92,8 +92,20 @@ ADD CONSTRAINT CHK_products_unit_price CHECK(unit_price > 0);
 -- 12. "Навесить" автоинкрементируемый счётчик на поле product_id
 -- таблицы products (БД northwind). Счётчик должен начинаться с числа
 -- следующего за максимальным значением по этому столбцу.
+SELECT MAX(product_id)
+FROM products;
 
+CREATE SEQUENCE IF NOT EXISTS products_product_id_seq
+START WITH 78 OWNED BY products.product_id;
+
+ALTER TABLE products
+ALTER COLUMN product_id SET DEFAULT nextval('products_product_id_seq');
 -- 13. Произвести вставку в products (не вставляя идентификатор явно)
 -- и убедиться, что автоинкремент работает.
 -- Вставку сделать так, чтобы в результате команды вернулось значение,
 -- сгенерированное в качестве идентификатора.
+INSERT INTO products( product_name, supplier_id, category_id,
+                     quantity_per_unit, unit_price,
+                     units_in_stock, units_on_order, reorder_level, discontinued)
+VALUES ('name', 1, 1, 10, 20, 20, 10, 1, 0)
+RETURNING product_id;
