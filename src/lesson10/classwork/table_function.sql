a93cf7a16b6bf641a658a0ec4439da07f6b1d8f7
TatianaKoma <tatiana78977ua@gmail.com>
2021-09-13 00:14:55 +0300

    lesson10 was started

---
+++ src/lesson10/classwork/table_function.sql
@@ -0,0 +1,35 @@
+SELECT *
+FROM customers;
+
+SELECT *
+INTO tmp_customers
+FROM customers;
+
+SELECT *
+FROM tmp_customers;
+
+UPDATE tmp_customers
+SET region = 'unknown'
+WHERE region IS NULL;
+
+CREATE OR REPLACE FUNCTION fix_customer_region() RETURNS void AS $$
+    UPDATE tmp_customers
+    SET region = 'unknown'
+    WHERE region IS NULL
+$$ LANGUAGE SQL;
+
+SELECT fix_customer_region();
+
+CREATE OR REPLACE FUNCTION get_total_numbers_of_goods() RETURNS BIGINT AS $$
+    SELECT SUM(units_in_stock)
+    FROM products
+    $$ LANGUAGE SQL;
+
+SELECT get_total_numbers_of_goods() AS total_goods;
+
+CREATE OR REPLACE FUNCTION get_avg_price() RETURNS FLOAT8 AS $$
+    SELECT AVG(unit_price)
+    FROM products
+    $$ LANGUAGE SQL;
+
+SELECT get_avg_price() AS avg_price;
