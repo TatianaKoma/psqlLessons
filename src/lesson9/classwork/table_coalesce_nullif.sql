7d65d711deefd1e3576af52cc63de28f635438c1
TatianaKoma <72659644+TatianaKoma@users.noreply.github.com>
2021-09-13 22:51:12 +0300

    Lesson9 (#9)
    * start lesson8
    
    * classwork from lesson8 was finished
    
    * lesson9 about case
    
    * lesson9 about case and coalesce was finished
    
    * lesson9 was changed after comments

---
+++ src/lesson9/classwork/table_coalesce_nullif.sql
@@ -0,0 +1,37 @@
+
+SELECT
+    order_id,
+    order_date,
+    COALESCE(ship_region, 'unknown') AS ship_region
+FROM orders
+LIMIT 10;
+
+SELECT
+    last_name,
+    first_name,
+    COALESCE(region, 'N/A') AS region
+FROM employees;
+
+SELECT contact_name,
+       COALESCE(NULLIF(city, ''), 'Unknown') AS city
+FROM customers;
+
+CREATE TABLE budgets
+(
+    department BIGSERIAL,
+    current_year DECIMAL,
+    previous_year DECIMAL
+);
+
+INSERT INTO budgets(current_year, previous_year)
+VALUES (100000, 150000),
+       (NULL, 150000),
+       (0, 100000),
+       (300000, 250000),
+       (170000, 170000),
+       (150000, NULL);
+SELECT
+    department,
+    COALESCE(TO_CHAR(NULLIF(current_year, previous_year), 'FM99999999'), 'Same as last year ') as budget
+FROM budgets
+WHERE current_year IS NOT NULL ;
