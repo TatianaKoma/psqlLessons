04977746d87a03a5fd2a7e77ed6eee12ae8ade2a
TatianaKoma <72659644+TatianaKoma@users.noreply.github.com>
2021-09-24 21:35:49 +0300

    Lesson10 (#10)
    * start lesson8
    
    * classwork from lesson8 was finished
    
    * lesson10 was started
    
    * lesson10 about in, out, setof
    
    * lesson10 about plpfsql and variables
    
    * lesson10 about if else and loops
    
    * lesson10 first part of homework
    
    * lesson10 second part of homework

---
+++ src/lesson10/classwork/table_PL_pgSQL.sql
@@ -0,0 +1,64 @@
+CREATE OR REPLACE FUNCTION get_total_number_of_goods()
+    RETURNS BIGINT AS $$
+        BEGIN
+            RETURN sum(units_in_stock)
+            FROM products;
+        END;
+    $$ LANGUAGE plpgsql;
+
+SELECT get_total_number_of_goods();
+
+CREATE OR REPLACE FUNCTION get_max_price_from_discontinued()
+    RETURNS REAL AS $$
+        BEGIN
+            RETURN max(unit_price)
+            FROM products
+            WHERE discontinued = 1;
+        END;
+    $$ LANGUAGE plpgsql;
+
+SELECT get_max_price_from_discontinued();
+
+CREATE OR REPLACE FUNCTION get_price_boundaries(OUT max_price REAL, OUT min_price REAL) AS $$
+    BEGIN
+        max_price := MAX(unit_price)
+        FROM products;
+        min_price := MIN(unit_price)
+        FROM products;
+    END;
+$$ LANGUAGE plpgsql;
+
+SELECT get_price_boundary_by_dscontinuity();
+
+
+CREATE OR REPLACE FUNCTION get_price_boundaries(OUT max_price REAL, OUT min_price REAL) AS $$
+    BEGIN
+        SELECT MAX(unit_price), MIN(unit_price)
+        INTO max_price, min_price
+        FROM products;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_sum(x INT, y INT, OUT result INT) AS $$
+    BEGIN
+        result := x + y;
+        RETURN;
+    END;
+$$ LANGUAGE plpgsql;
+
+SELECT *
+FROM get_sum(2,3);
+
+CREATE OR REPLACE FUNCTION get_customers_by_country(customer_country VARCHAR)
+    RETURNS SETOF customers AS $$
+       BEGIN
+           RETURN QUERY
+           SELECT *
+           FROM customers
+           WHERE country = customer_country;
+       END;
+    $$ LANGUAGE plpgsql;
+
+SELECT *
+FROM get_customers_by_country('USA');
+
---
+++ src/lesson10/classwork/table_in_out.sql
@@ -0,0 +1,49 @@
+CREATE OR REPLACE FUNCTION get_product_price_by_name(prod_name VARCHAR)
+    RETURNS REAL AS $$
+        SELECT unit_price
+        FROM products
+        WHERE product_name = prod_name
+    $$ LANGUAGE SQL;
+
+SELECT get_product_price_by_name('Chocolade') AS price;
+
+CREATE OR REPLACE FUNCTION get_price_bounaries(OUT max_price REAL, OUT min_price REAL) AS $$
+    SELECT MAX(unit_price), MIN(unit_price)
+    FROM products
+$$ LANGUAGE SQL;
+
+SELECT get_price_bounaries();
+
+SELECT *
+FROM get_price_bounaries();
+
+CREATE OR REPLACE FUNCTION get_price_boundary_by_dscontinuity
+    (IN is_discontinued INT, OUT max_price REAL, OUT min_price REAL) AS $$
+        SELECT MAX(unit_price), MIN(unit_price)
+        FROM products
+        WHERE discontinued = is_discontinued
+    $$ LANGUAGE SQL;
+
+SELECT get_price_boundary_by_dscontinuity(1);
+
+SELECT *
+FROM get_price_boundary_by_dscontinuity(1);
+
+ CREATE OR REPLACE FUNCTION get_price_boundary_by_dscontinuity
+    (IN is_discontinued INT DEFAULT 1, OUT max_price REAL, OUT min_price REAL) AS $$
+        SELECT MAX(unit_price), MIN(unit_price)
+        FROM products
+        WHERE discontinued = is_discontinued
+    $$ LANGUAGE SQL;
+
+ SELECT get_price_boundary_by_dscontinuity();
+
+SELECT *
+FROM get_price_boundary_by_dscontinuity();
+
+ SELECT *
+ FROM get_price_boundary_by_dscontinuity(0);
+
+ SELECT *
+ FROM get_price_boundary_by_dscontinuity(1);
+
